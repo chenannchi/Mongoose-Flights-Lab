4780fc5814cbe698ab4fae8d4508b7954538ea26
chenannchi <anchichenusa@gmail.com>
2022-09-26 09:37:34 -0400

    set default to date

--- models/flight.js
+++ models/flight.js
@@ -20,11 +20,22 @@ const flightSchema = new Schema({
   },
   departs: {
     type: Date,
-    default: Date.now,
+    // default: Date.now,
+    default:oneYearFromNow
   }
 }, {
   timestamps: true
 })
+
+function oneYearFromNow(){
+  const today = new Date()
+  // console.log(today.getFullYear()+1)
+  today.setFullYear(today.getFullYear()+1)
+  // console.log(today)
+  return today
+}
+
+oneYearFromNow()
 
 // Compile the schema into a model and export it
 const Flight = mongoose.model('Flight', flightSchema)
